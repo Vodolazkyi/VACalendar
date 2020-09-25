384eb438903ef28b603169ca3a8cd48fdde8e6e3
Fidel Morales <fidmor.89@outlook.com>
2020-09-25 11:42:39 -0600

    Remove unneeded use of optional

--- VACalendar/VACalendarView.swift
+++ VACalendar/VACalendarView.swift
@@ -202,15 +202,15 @@ public class VACalendarView: UIScrollView {
         }
     }
     
-    public func scrollToStartDate(startDate: Date? = Date()) {
-        let startMonth = monthViews.first(where: { $0.month.dateInThisMonth(startDate ?? Date()) })
+    public func scrollToStartDate(startDate: Date = Date()) {
+        let startMonth = monthViews.first(where: { $0.month.dateInThisMonth(startDate) })
         var offset: CGPoint = startMonth?.frame.origin ?? .zero
         
         setContentOffset(offset, animated: false)
         drawVisibleMonth(with: contentOffset)
         
         if viewType == .week {
-            let weekOffset = startMonth?.week(with: startDate ?? Date())?.frame.origin.x ?? 0
+            let weekOffset = startMonth?.week(with: startDate)?.frame.origin.x ?? 0
             let inset = startMonth?.monthViewAppearanceDelegate?.leftInset?() ?? 0
             offset.x += weekOffset - inset
             setContentOffset(offset, animated: false)
